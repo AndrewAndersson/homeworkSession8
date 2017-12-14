56e288e1f80a9d250b1c7525b8f1ab820dfe92c3
AndrewAndersson <anderssona258@gmail.com>
2017-12-14 21:44:33 +0200

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var total = 0;
+const result = document.getElementById('result');
 for (i = 1; i <= 15; i++) {
     var first = Math.floor((Math.random() * 6) + 1);
     var second = Math.floor((Math.random() * 6) + 1);
@@ -7,16 +8,16 @@ for (i = 1; i <= 15; i++) {
     }
 
     if (first == second) {
-        document.getElementById('result').innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Выпал дубль. Число: " + first + " ";
+        result.innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Выпал дубль. Число: " + first + " ";
     } else if ((first < 3 && second > 4) || (first > 4 && second < 3)) {
         var difference = 0;
         difference = Math.abs(second - first);
-        document.getElementById("result").innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Большой разброс между костями. Разница составляет: " + difference + " ";
+        result.innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Большой разброс между костями. Разница составляет: " + difference + " ";
     } else {
-        document.getElementById('result').innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Первая кость: " + first + " " + "Вторая кость: " + second + " ";
+        result.innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Первая кость: " + first + " " + "Вторая кость: " + second + " ";
     }
 
     total += first + second;
 
 }
-document.getElementById('result').innerHTML += (total >= 100 ? "<br />Победа, вы набрали" + " " + total + " " + "очков" : "<br />Вы проиграли, у вас" + " " + total + " " + "очков");
+result.innerHTML += (total >= 100 ? "<br />Победа, вы набрали" + " " + total + " " + "очков" : "<br />Вы проиграли, у вас" + " " + total + " " + "очков");
