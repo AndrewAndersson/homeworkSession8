cc4dfa23ba3064177e151c030e461710f6790186
AndrewAndersson <anderssona258@gmail.com>
2017-12-16 14:55:46 +0200

    corrections after parsing

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-var total = 0;
-const result = document.getElementById('result');
-for (i = 1; i <= 15; i++) {
-    var first = Math.floor((Math.random() * 6) + 1);
-    var second = Math.floor((Math.random() * 6) + 1);
+'use strict';
+var total = 0,
+    first,
+    second,
+    result = "";
+const element = document.getElementById('result');
+
+for (let i = 1; i <= 15; i++) {
     if (i == 8 || i == 13) {
         continue;
     }
 
-    if (first == second) {
-        result.innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Выпал дубль. Число: " + first + " ";
-    } else if ((first < 3 && second > 4) || (first > 4 && second < 3)) {
-        var difference = 0;
-        difference = Math.abs(second - first);
-        result.innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Большой разброс между костями. Разница составляет: " + difference + " ";
-    } else {
-        result.innerHTML += "ИТЕРАЦИЯ: " + i + " " + "Первая кость: " + first + " " + "Вторая кость: " + second + " ";
+    first = Math.floor((Math.random() * 6) + 1);
+    second = Math.floor((Math.random() * 6) + 1);
+    result += "Первая кость: " + first + " " + "Вторая кость: " + second + "<br>";
+
+    if (first === second) {
+        result += "Выпал дубль. Число: " + first + "<br>";
+    }
+    if ((first < 3 && second > 4) || (first > 4 && second < 3)) {
+        result += "Большой разброс между костями. Разница составляет: " + Math.abs(second - first) + "<br>";
     }
 
     total += first + second;
 
 }
-result.innerHTML += (total >= 100 ? "<br />Победа, вы набрали" + " " + total + " " + "очков" : "<br />Вы проиграли, у вас" + " " + total + " " + "очков");
+result += (total >= 100) ? "<br />Победа, вы набрали" + " " + total + " " + "очков" : "<br />Вы проиграли, у вас" + " " + total + " " + "очков";
+element.innerHTML = result;
